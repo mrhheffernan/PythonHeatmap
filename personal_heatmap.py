b37d817ce456c7b6c43ec8593ee81e9e7a4fe4e3
Matthew Heffernan <mrheffernan@email.wm.edu>
2018-11-03 15:12:02 -0400

    Partial opacity working

--- personal_heatmap.py
+++ personal_heatmap.py
@@ -18,16 +18,14 @@ print('data = ',data)
 lat = []
 lon = []
 
-fig = plt.figure(facecolor = '0.05')
-ax = plt.Axes(fig, [0., 0., 1., 1.], )
-ax.set_aspect('equal')
-ax.set_axis_off()
-fig.add_axes(ax)
+# fig = plt.figure(facecolor = '0.05')
+# ax = plt.Axes(fig, [0., 0., 1., 1.], )
+# ax.set_aspect('equal')
+# ax.set_axis_off()
+# fig.add_axes(ax)
 
 all_lat = []
 all_long = []
-
-m = folium.Map(location=[central_lat,central_long],tiles="Stamen Toner",zoom_start=15)
 
 for activity in data:
     gpx_filename = activity#join(data_path,activity)
@@ -39,13 +37,11 @@ for activity in data:
             for point in segment.points:
                 lat.append(point.latitude)
                 lon.append(point.longitude)
-    #plt.plot(lon, lat, color = 'deepskyblue', lw = 0.2, alpha = 0.8)
     all_lat.append(lat)
     all_long.append(lon)
-    points = zip(lat,lon)
-    folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(my_map)
     lat = []
     lon = []
+
 all_lat = all_lat[0]
 all_long = all_long[0]
 
@@ -65,5 +61,26 @@ print('min lat',max(all_long))
 
 central_long = sum(all_long)/float(len(all_long))
 central_lat = sum(all_lat)/float(len(all_lat))
+
+
+m = folium.Map(location=[central_lat,central_long],tiles="Stamen Toner",zoom_start=15)
+
+for activity in data:
+    gpx_filename = activity#join(data_path,activity)
+    gpx_file = open(gpx_filename, 'r')
+    gpx = gpxpy.parse(gpx_file)
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                lat.append(point.latitude)
+                lon.append(point.longitude)
+    #plt.plot(lon, lat, color = 'deepskyblue', lw = 0.2, alpha = 0.8)
+    points = zip(lat,lon)
+    points = [item for item in zip(lat,lon)]
+    #print('points =',points)
+    folium.PolyLine(points, color="red", weight=2.5, opacity=0.5).add_to(m)
+    lat = []
+    lon = []
 
 m.save('heatmap.html')
