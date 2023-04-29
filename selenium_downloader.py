71a63f3255d6c4f710df49145bef3242d398de22
Matthew Heffernan <mrhheffernan@gmail.com>
2023-04-28 21:14:23 -0700

    Enforcing black formatting, adding CI

--- selenium_downloader.py
+++ selenium_downloader.py
@@ -1,31 +1,34 @@
 from selenium import webdriver
 import time
-driver = webdriver.Chrome(executable_path='<replace>')
 
-download_dir = '<replace>'
+driver = webdriver.Chrome(executable_path="<replace>")
+
+download_dir = "<replace>"
 
 options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
+options.add_argument("--ignore-certificate-errors")
 options.add_argument("--test-type")
 options.binary_location = "/usr/bin/chromium-browser"
 
-options.add_experimental_option('prefs', {
-    "plugins.plugins_list": [{"enabled":False,"name":"Chrome PDF Viewer"}],
-    "download": {
-        "prompt_for_download": False,
-        "default_directory"  : download_dir
-    }
-})
+options.add_experimental_option(
+    "prefs",
+    {
+        "plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
+        "download": {"prompt_for_download": False, "default_directory": download_dir},
+    },
+)
 driver = webdriver.Chrome(chrome_options=options)
 
-usern, passw, id = open('login_info.secret').read().strip().split(',')
+usern, passw, id = open("login_info.secret").read().strip().split(",")
 
-driver.get('http://strava.com/login')
+driver.get("http://strava.com/login")
 
 usern_box = driver.find_element_by_xpath("//input[@name='email' and @type='email']")
 usern_box.send_keys(usern)
 
-passw_box = driver.find_element_by_xpath("//input[@name='password' and @type='password']")
+passw_box = driver.find_element_by_xpath(
+    "//input[@name='password' and @type='password']"
+)
 passw_box.send_keys(passw)
 
 submit_button = driver.find_element_by_xpath('//button[@id="login-button"]')
@@ -33,14 +36,18 @@ submit_button.click()
 
 time.sleep(2)
 
-driver.get("https://www.strava.com/athletes/"+str(id))
+driver.get("https://www.strava.com/athletes/" + str(id))
 
-monthly_button = driver.find_element_by_xpath('//a[contains(@class,"button btn-xs") and contains(@href,"month")]')
+monthly_button = driver.find_element_by_xpath(
+    '//a[contains(@class,"button btn-xs") and contains(@href,"month")]'
+)
 monthly_button.click()
 
 time.sleep(2)
 
-bar_list = driver.find_elements_by_xpath('//a[@class="bar" and contains(@href,"interval")]')
+bar_list = driver.find_elements_by_xpath(
+    '//a[@class="bar" and contains(@href,"interval")]'
+)
 
 activity_list = []
 
@@ -48,17 +55,19 @@ for bar in bar_list:
     bar.click()
     time.sleep(3)
 
-    for a in driver.find_elements_by_xpath('.//a[contains(@href, "activities") and not(contains(@href, "twitter")) and not(contains(@href, "#")) and not(contains(@href, "photos")) and not(contains(@href, "segments"))]'):
-         activity_list.append(a.get_attribute('href'))
+    for a in driver.find_elements_by_xpath(
+        './/a[contains(@href, "activities") and not(contains(@href, "twitter")) and not(contains(@href, "#")) and not(contains(@href, "photos")) and not(contains(@href, "segments"))]'
+    ):
+        activity_list.append(a.get_attribute("href"))
 
 activity_list = set(activity_list)
 
-print('Number of activities found: ',len(activity_list))
+print("Number of activities found: ", len(activity_list))
 
 for address in activity_list:
-    driver.get(address+"/export_gpx")
+    driver.get(address + "/export_gpx")
     time.sleep(1)
 
-print('Data downloaded to '+download_dir+', quitting.')
+print("Data downloaded to " + download_dir + ", quitting.")
 
 driver.quit()
--- simple_heatmap.py
+++ simple_heatmap.py
@@ -2,26 +2,28 @@ import gpxpy
 import matplotlib.pyplot as plt
 import glob
 
-gpx_list = glob.glob('*.gpx')
+gpx_list = glob.glob("*.gpx")
 
 
-
-fig = plt.figure(facecolor = '0.05')
-ax = plt.Axes(fig, [0., 0., 1., 1.], )
-ax.set_aspect('equal')
+fig = plt.figure(facecolor="0.05")
+ax = plt.Axes(
+    fig,
+    [0.0, 0.0, 1.0, 1.0],
+)
+ax.set_aspect("equal")
 ax.set_axis_off()
 fig.add_axes(ax)
 
 for gpx_data in gpx_list:
     lat = []
     lon = []
-    gpx_file = open(gpx_data, 'r')
+    gpx_file = open(gpx_data, "r")
     gpx = gpxpy.parse(gpx_file)
     for track in gpx.tracks:
         for segment in track.segments:
             for point in segment.points:
                 lat.append(point.latitude)
                 lon.append(point.longitude)
-    plt.plot(lon, lat, color = 'deepskyblue', lw = 0.8, alpha = 0.8)
-#plt.savefig('simple_heatmap.png')
+    plt.plot(lon, lat, color="deepskyblue", lw=0.8, alpha=0.8)
+# plt.savefig('simple_heatmap.png')
 plt.show()
