bd4e25b254d3856e958cd5fc964f62d1ed2ee813
Matthew Heffernan <mrhheffernan@gmail.com>
2026-03-07 22:29:26 -0800

    Clean up simple_heatmap, make directory and output path configurable

--- simple_heatmap.py
+++ simple_heatmap.py
@@ -1,30 +1,56 @@
+import argparse
 import glob
+import os
 
 import gpxpy
 import matplotlib.pyplot as plt
 
-gpx_list = glob.glob("*.gpx")
+
+def parse_args() -> argparse.Namespace:
+    args = argparse.ArgumentParser(description="Create a simple heatmap")
+
+    args.add_argument(
+        "--dir",
+        help="Path to directory containing files to parse to generate the heatmap",
+        default=os.getcwd(),
+    )
+    args.add_argument(
+        "--output_path",
+        help="Path to output the simple heatmap",
+        default="simple_heatmap.png",
+    )
+
+    return args.parse_args()
 
 
-fig = plt.figure(facecolor="0.05")
-ax = plt.Axes(
-    fig,
-    [0.0, 0.0, 1.0, 1.0],
-)
-ax.set_aspect("equal")
-ax.set_axis_off()
-fig.add_axes(ax)
+def main():
+    args = parse_args()
+    
+    gpx_list = glob.glob(args.dir + "/*.gpx")
 
-for gpx_data in gpx_list:
-    lat = []
-    lon = []
-    with open(gpx_data, "r") as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    lat.append(point.latitude)
-                    lon.append(point.longitude)
-    plt.plot(lon, lat, color="deepskyblue", lw=0.8, alpha=0.8)
-# plt.savefig('simple_heatmap.png')
-plt.show()
+    fig = plt.figure(facecolor="0.05")
+    ax = plt.Axes(
+        fig,
+        [0.0, 0.0, 1.0, 1.0],
+    )
+    ax.set_aspect("equal")
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+    for gpx_data in gpx_list:
+        lat = []
+        lon = []
+        with open(gpx_data, "r") as gpx_file:
+            gpx = gpxpy.parse(gpx_file)
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    for point in segment.points:
+                        lat.append(point.latitude)
+                        lon.append(point.longitude)
+        plt.plot(lon, lat, color="deepskyblue", lw=0.8, alpha=0.8)
+    plt.savefig(args.output_path)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
